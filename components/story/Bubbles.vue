28d61ad77ae7bcf3b6b08021a64e63039db4eece
ToMattBan <souaquelecleyton@yandex.com>
2023-04-30 17:02:23 -0300

    Starting story

---
+++ components/story/Bubbles.vue
@@ -0,0 +1,58 @@
+<template>
+  <article class="message" :class="{ startRight: character == 'char' }">
+    <div class="char_picture">
+      <img :src="`/chars/${character}.jpeg`" />
+    </div>
+
+    <span class="bubble">
+      <slot />
+    </span>
+  </article>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  character: String,
+});
+</script>
+
+<style scoped lang="scss">
+.message {
+  margin: var(--small) 0;
+  display: flex;
+
+  &.startRight {
+    flex-direction: row-reverse;
+
+    .char_picture {
+      margin-right: 0;
+      margin-left: var(--small);
+    }
+
+    .bubble {
+      border-radius: var(--borderRadius) var(--borderRadius) 0
+        var(--borderRadius);
+    }
+  }
+
+  .char_picture {
+    width: 50px;
+    height: 50px;
+    overflow: hidden;
+    border-radius: 50%;
+    margin-right: var(--small);
+
+    img {
+      width: 100%;
+    }
+  }
+
+  .bubble {
+    display: inline-block;
+    padding: var(--small);
+    background-color: var(--blueDark);
+    color: var(--pureWhite);
+    border-radius: var(--borderRadius) var(--borderRadius) var(--borderRadius) 0;
+  }
+}
+</style>
